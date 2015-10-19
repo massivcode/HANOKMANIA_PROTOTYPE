d724e19d6bb31af6ea8b2a2a74d9919c43ac43d9
prChoe <massivcode@gmail.com>
2015-10-19 09:10:19 +0900

    불필요한 코드 제거

--- app/src/main/java/com/raychoe/httpexample/MainActivity.java
+++ app/src/main/java/com/raychoe/httpexample/MainActivity.java
@@ -8,45 +8,19 @@ import com.raychoe.httpexample.HanokRepairAdvice.HanokRepairAdvice;
 import com.raychoe.httpexample.HanokRepairAdvice.HanokRepairAdviceFacade;
 import com.raychoe.httpexample.HanokStatus.HanokStatus;
 import com.raychoe.httpexample.HanokStatus.HanokStatusFacade;
-import com.raychoe.httpexample.Utils.HanokDBHelper;
 import com.raychoe.httpexample.Utils.JsonToSqlite;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private HanokDBHelper mHelper;
     private HanokStatusFacade mHanokStatusFacade;
     private HanokRepairAdviceFacade mHanokRepairAdviceFacade;
-
-    // 클라이언트 오브젝트
-    private OkHttpClient client = new OkHttpClient();
-
-    /**
-     * url 로 부터 스트림을 읽어 String 으로 반환한다
-     * @param url
-     * @return
-     * @throws IOException
-     */
-    private String getResponse(String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        Response response = client.newCall(request).execute();
-        return response.body().string();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mHelper = HanokDBHelper.getInstance(MainActivity.this);
         mHanokStatusFacade = new HanokStatusFacade(MainActivity.this);
         mHanokRepairAdviceFacade = new HanokRepairAdviceFacade(MainActivity.this);
 
